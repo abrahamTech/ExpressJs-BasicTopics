e6b9f19e44e601f6c88281dbecef5ece0f0d772e
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-07-23 22:36:02 -0600

    feat(11ExpressRestAPICrud): pricipal routes have been added

--- 11ExpressRestAPICrud/server.js
+++ 11ExpressRestAPICrud/server.js
@@ -1,1 +1,32 @@
-console.log("First Configurations READY")
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("dev"));
+// app.use(morgan("short"));
+// app.use(morgan("tiny"));
+
+app.get("/products", (req, res) => {
+    res.send("Getting Products");
+});
+
+app.post("/products", (req, res) => {
+    res.send("Creating Products");
+});
+
+app.put("/products", (req, res) => {
+    res.send("Updating Products");
+});
+
+app.delete("/products", (req, res) => {
+    res.send("Deleting Products");
+});
+
+app.get("/products/:id", (req, res) => {
+    res.send("Getting One Product");
+});
+
+app.listen(3000, () => {
+    console.log(`Server listening on port ${3000}`);
+});
