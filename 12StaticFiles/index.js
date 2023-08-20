5d3562e36d47329742a80de2a522f4b93b82f608
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-08-19 19:53:55 -0600

    feat(12StaticFiles): get access to all files in public and uploads folders but with a different way of accessing

--- 12StaticFiles/index.js
+++ 12StaticFiles/index.js
@@ -10,17 +10,23 @@ app.set("port", 3000);
 app.use(express.json());
 app.use(morgan("dev"));
 
-//You can access to all in "public" folder
-app.use(express.static("./public"));
-//You can acces to: http://localhost:3000/index.css
-
 
 //Routes
 
 //Path with the same name as a file in the "public" folder
-app.get("/.note.txt", (req, res) => {
+app.get("/note.txt", (req, res) => {
     res.send("This isn't a file, is a ROUTE");
 });
+
+
+//You can access to all in "public" folder
+//app.use(express.static("./public"));
+//You can acces to: http://localhost:3000/note.txt
+
+//You can access to all in "public" folder but you need first to write public in the path
+app.use("/public", express.static("./public"));
+//You can acces to: http://localhost:3000/public/note.txt
+app.use("/uploads", express.static("./uploads"));
 
 
 app.listen(app.get("port"), ()=>{
